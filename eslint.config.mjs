6da865de2283cb4e2f4866beb6b96bc420113174
KALalev18 <59725875+KALalev18@users.noreply.github.com>
2025-03-03 19:50:31 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,30 @@
-export default {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
-  plugins: [
-    'react'
-  ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
-};
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-console":
+        import.meta.env?.NODE_ENV === "production" ? "error" : "off",
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ["/*.{jsx,tsx}"], // Ensure React rules apply only to JSX/TSX files
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/prop-types": "off", // Example rule
+      "react/jsx-uses-react": "off", // Example rule
+      "react/jsx-uses-vars": "error", // Example rule
+      // Add more React-related rules or override as needed
+    },
+  },
+];
